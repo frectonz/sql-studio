a902aa087917db706cd45bbada9946bb5ec9fe1e
frectonz <fraol0912@gmail.com>
2024-06-11 00:30:41 +0300

    feat: setup shadcn/ui

--- ui/src/routes/__root.tsx
+++ ui/src/routes/__root.tsx
@@ -1,17 +1,93 @@
+import { Menu, Database } from "lucide-react";
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+
 export const Route = createRootRoute({
-  component: () => (
+  component: Root,
+});
+
+export function Root() {
+  return (
     <>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Overview
-        </Link>
+      <div className="flex min-h-screen w-full flex-col">
+        <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+          <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+            <Link
+              to="/"
+              className="flex items-center gap-2 text-lg font-semibold md:text-base"
+            >
+              <Database className="h-6 w-6" />
+              <span className="sr-only">SQLite Studio</span>
+            </Link>
+            <Link
+              to="/"
+              className="[&.active]:text-foreground text-muted-foreground transition-colors hover:text-foreground"
+            >
+              Overview
+            </Link>
+            <Link
+              href="/"
+              className="[&.active]:text-foreground text-muted-foreground transition-colors hover:text-foreground"
+            >
+              Tables
+            </Link>
+            <Link
+              href="/"
+              className="[&.active]:text-foreground text-muted-foreground transition-colors hover:text-foreground"
+            >
+              Query
+            </Link>
+          </nav>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button
+                variant="outline"
+                size="icon"
+                className="shrink-0 md:hidden"
+              >
+                <Menu className="h-5 w-5" />
+                <span className="sr-only">Toggle navigation menu</span>
+              </Button>
+            </SheetTrigger>
+            <SheetContent side="left">
+              <nav className="grid gap-6 text-lg font-medium">
+                <Link
+                  href="#"
+                  className="flex items-center gap-2 text-lg font-semibold"
+                >
+                  <Database className="h-6 w-6" />
+                  <span className="sr-only">Acme Inc</span>
+                </Link>
+                <Link
+                  href="/"
+                  className="[&.active]:text-foreground text-muted-foreground hover:text-foreground"
+                >
+                  Overview
+                </Link>
+                <Link
+                  href="/"
+                  className="[&.active]:text-foreground text-muted-foreground hover:text-foreground"
+                >
+                  Tables
+                </Link>
+                <Link
+                  href="/"
+                  className="[&.active]:text-foreground text-muted-foreground hover:text-foreground"
+                >
+                  Query
+                </Link>
+              </nav>
+            </SheetContent>
+          </Sheet>
+        </header>
+        <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+          <Outlet />
+        </main>
       </div>
-      <hr />
-      <Outlet />
       <TanStackRouterDevtools />
     </>
-  ),
-});
+  );
+}
--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,7 +1,7 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
-import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
