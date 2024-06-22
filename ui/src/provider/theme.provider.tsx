28a7a6ac3404d602fba82810b1651e5a862b84ac
frectonz <fraol0912@gmail.com>
2024-06-22 21:32:11 +0300

    chore: run formatter

--- ui/src/provider/theme.provider.tsx
+++ ui/src/provider/theme.provider.tsx
@@ -4,20 +4,19 @@ export type Theme = "dark" | "light";
 
 function initialState(): Theme {
   if (localStorage.getItem("theme") !== null) {
-    return localStorage.getItem("theme") === "dark" ? "dark" : "light"
+    return localStorage.getItem("theme") === "dark" ? "dark" : "light";
   }
 
-  return window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light"
+  return window.matchMedia("(prefers-color-scheme: dark)").matches
+    ? "dark"
+    : "light";
 }
 
 export const {
   StoreProvider: ThemeProvider,
   useDispatch: setTheme,
   useStore: useTheme,
-} = createStore(
-  (_, next: Theme) => {
-    localStorage.setItem("theme", next);
-    return next;
-  },
-  initialState(),
-);
+} = createStore((_, next: Theme) => {
+  localStorage.setItem("theme", next);
+  return next;
+}, initialState());
